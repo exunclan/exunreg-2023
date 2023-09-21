85a419d14f4d5a72509665c9f2a3aca4eb37bcf4
Tejas Agarwal <tejasagarwalbly@gmail.com>
2023-09-21 21:15:09 +0530

    chore: prod debugging logs

--- app/(website)/dashboard/page.tsx
+++ app/(website)/dashboard/page.tsx
@@ -5,6 +5,8 @@ import { redirect } from "next/navigation";
 
 export default function RegisterPage() {
   const { data: session, status } = useSession();
+  console.log(status);
+  console.log(session);
 
   if (status === "authenticated") return <>Register</>;
   else if (status === "loading") return <>loading</>;
