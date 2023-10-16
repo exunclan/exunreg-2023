c10836f3951a92b839e0a2069ddfae38d279cfc4
AyonC <ayon@github.com>
2023-10-16 23:09:57 +0530

    add: about button, fix: image

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -6,6 +6,7 @@ import { fetchEvents, Images, Groups } from "@/util/data/Events";
 import { useQuery } from "@tanstack/react-query";
 import Loading from "@/components/Loading";
 import Error from "@/components/Error";
+import Image from "next/image";
 
 export default function HomePage() {
   const {
@@ -33,15 +34,16 @@ export default function HomePage() {
     <>
       {/* Hero */}
       <div className="h-[75vh] w-full flex flex-col justify-center items-center">
-        <div className="text-main text-center text-3xl sm:text-5xl font-semibold m-4">
-          The biggest tech symposium of India
-        </div>
-        <div className="flex-1 bg-red">
-          <img
+        <div className="flex md:flex-col flex-col-reverse justify-center items-center">
+          <div className="text-main text-center text-3xl sm:text-5xl font-semibold m-4">
+            The biggest tech symposium of India
+          </div>
+          <Image
             src="/illustration.png"
             alt="Illustration"
-            height="400"
             width="400"
+            height="400"
+            className="w-40 sm:w-[400px]"
           />
         </div>
         <div className="text-accent-light text-sm sm:text-lg max-w-xs sm:max-w-3xl text-center">
