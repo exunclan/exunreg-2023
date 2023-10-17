20701bba76c5d2c5c2bf9b00f39ab020910dde2d
aludayalu <pl46u3.ducklinz@gmail.com>
2023-10-17 20:54:35 +0530

    added back events tab

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -23,6 +23,10 @@ export default function WebsiteLayout({
               link: "https://exunclan.com",
             },
             {
+              name: "Events",
+              link: "https://docs.google.com/document/u/6/d/e/2PACX-1vT1q_W-T6QnQLEm5B3onr7QDfLVOa5yglBAsPYbuA0JCHFQmQZDtteOpQNcn5eAm4ovhemXul6kxaII/pub",
+            },
+            {
               name: "Sign In",
               link: "/user/signin",
             },
