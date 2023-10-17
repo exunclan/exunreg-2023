b08d00d5ad8c45e78fd63c7ffa63a5ca60a1680c
aludayalu <pl46u3.ducklinz@gmail.com>
2023-10-17 19:37:25 +0530

    add button

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -58,8 +58,13 @@ export default function HomePage() {
         </div>
       </div>
 
-      <div className="justify-center items-center mt-[1rem]" style={{width:"100vw",height:"100vh"}}>
-        <iframe src="https://docs.google.com/document/u/6/d/e/2PACX-1vT1q_W-T6QnQLEm5B3onr7QDfLVOa5yglBAsPYbuA0JCHFQmQZDtteOpQNcn5eAm4ovhemXul6kxaII/pub" style={{width:"100vw",height:"100vh"}}></iframe>
+      <div className="flex flex-col justify-center items-center mt-[1rem]" style={{width:"100vw",height:"100vh"}}>
+        <button
+            className="my-2 bg-main p-2 rounded-md text-white"
+            onClick={()=>{window.open("https://docs.google.com/document/u/6/d/e/2PACX-1vT1q_W-T6QnQLEm5B3onr7QDfLVOa5yglBAsPYbuA0JCHFQmQZDtteOpQNcn5eAm4ovhemXul6kxaII/pub","_blank")}}
+          >
+          Event Details
+        </button>
       </div>
     </>
   );
