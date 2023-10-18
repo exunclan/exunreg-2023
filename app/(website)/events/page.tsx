d74fd0831112e2b4f7be9bdf3e2ecfeb5564ce31
Aarav Dayal <97356263+aludayalu@users.noreply.github.com>
2023-10-18 14:24:50 +0530

    Update page.tsx

--- app/(website)/events/page.tsx
+++ app/(website)/events/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-import { useRouter } from 'next/router'
 import { useEffect } from "react"
 
 export default function EventsPage() {
-  const router=useRouter()
   useEffect(()=>{
     setTimeout(()=>{
-      router.push("https://docs.google.com/document/u/6/d/e/2PACX-1vT1q_W-T6QnQLEm5B3onr7QDfLVOa5yglBAsPYbuA0JCHFQmQZDtteOpQNcn5eAm4ovhemXul6kxaII/pub")
+      window.location=("https://docs.google.com/document/u/6/d/e/2PACX-1vT1q_W-T6QnQLEm5B3onr7QDfLVOa5yglBAsPYbuA0JCHFQmQZDtteOpQNcn5eAm4ovhemXul6kxaII/pub")
     },100)
   })
   return (<>
