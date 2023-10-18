10e5c992dd3fc19734f25184f76a3df16978bfbb
Aarav Dayal <97356263+aludayalu@users.noreply.github.com>
2023-10-18 14:27:55 +0530

    Type safety error fix
    TS

--- app/(website)/events/page.tsx
+++ app/(website)/events/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 export default function EventsPage() {
   useEffect(()=>{
     setTimeout(()=>{
-      window.location=("https://docs.google.com/document/u/6/d/e/2PACX-1vT1q_W-T6QnQLEm5B3onr7QDfLVOa5yglBAsPYbuA0JCHFQmQZDtteOpQNcn5eAm4ovhemXul6kxaII/pub")
+      window.location.href="https://docs.google.com/document/u/6/d/e/2PACX-1vT1q_W-T6QnQLEm5B3onr7QDfLVOa5yglBAsPYbuA0JCHFQmQZDtteOpQNcn5eAm4ovhemXul6kxaII/pub"
     },100)
   })
   return (<>
