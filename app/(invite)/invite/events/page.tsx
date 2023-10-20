26a3525ce4d018d2d86a4e27f41b0f4ba089a7ab
Aarav Dayal <97356263+aludayalu@users.noreply.github.com>
2023-10-20 16:07:53 +0530

    Redirecting on events/invite to docs.google.com/...

--- app/(invite)/invite/events/page.tsx
+++ app/(invite)/invite/events/page.tsx
@@ -1,79 +1,12 @@
 "use client";
-
-import { IEvent, fetchEvents } from "@/util/data/Events";
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import EventDescription from "@/components/EventDescription";
-import Loading from "@/components/Loading";
-import Error from "@/components/Error";
+import { useEffect } from "react"
 
 export default function EventsPage() {
-  const {
-    isLoading,
-    error,
-    data: Events,
-  } = useQuery({
-    queryKey: ["events"],
-    queryFn: fetchEvents,
-  });
-  const [query, setQuery] = useState<string>("");
-
-  if (isLoading) return <Loading />;
-
-  if (!Events || error) return <Error />;
-
-  return (
-    <div className="my-[4rem] mx-[2rem] md:mx-[9rem]">
-      <div className="text-main text-5xl md:text-6xl font-semibold">Events</div>
-      <div className="input-group my-5">
-        <input
-          type="text"
-          placeholder="Search..."
-          onChange={(e) => {
-            setQuery(e.target.value as string);
-          }}
-        />
-      </div>
-      <div>
-        {Events.filter((obj: { [key: string]: any }) =>
-          Object.keys(obj).some(
-            (key) =>
-              typeof obj[key] === "string" &&
-              obj[key].toLowerCase().includes(query.toLowerCase())
-          )
-        ).map(
-          (
-            {
-              name,
-              classes,
-              participants,
-              teams,
-              independent,
-              registrations,
-              image,
-              mode,
-              summary,
-              description,
-            },
-            i
-          ) => (
-            <EventDescription
-              key={i}
-              idx={i + 1}
-              name={name}
-              classes={classes}
-              participants={participants}
-              teams={teams}
-              independent={independent}
-              registrations={registrations}
-              mode={mode}
-              image={image}
-              summary={summary}
-              description={description}
-            />
-          )
-        )}
-      </div>
-    </div>
-  );
+  useEffect(()=>{
+    setTimeout(()=>{
+      window.location.href="https://docs.google.com/document/u/6/d/e/2PACX-1vT1q_W-T6QnQLEm5B3onr7QDfLVOa5yglBAsPYbuA0JCHFQmQZDtteOpQNcn5eAm4ovhemXul6kxaII/pub"
+    },100)
+  })
+  return (<>
+  </>)
 }
