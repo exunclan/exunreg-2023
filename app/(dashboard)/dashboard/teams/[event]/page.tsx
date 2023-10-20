775487b5ecaa8e150cc0d858b9b3aef1337e5ae1
Tejas Agarwal <tejasagarwalbly@gmail.com>
2023-10-20 22:16:26 +0530

    fix: text

--- app/(dashboard)/dashboard/teams/[event]/page.tsx
+++ app/(dashboard)/dashboard/teams/[event]/page.tsx
@@ -28,9 +28,9 @@ export default async function DashboardEventTeamsPage({
       <div className="text-main text-4xl font-semibold text-center mx-6 mt-6 mb-2">
         {event} Team
       </div>
-      <div className="text-accent-light text-lg mb-4">
+      <div className="text-accent-light text-md mb-4">
         You are responsible for any incorrect data entered. Your data might be
-        shared with our sponsors for reference purposes.
+        shared with our sponsors.
       </div>
       <TeamData event={event} team={team} userId={session?.user?.id} />
     </Column>
